709a12ead180b3d6c95b1d7a9f15fb40b11c6a26
Ives Feng <97079241+Ivesfsy@users.noreply.github.com>
2023-11-07 15:33:34 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { getClientConfig } from "./config/client";
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "蓝衫ChatGPT 进阶版",
-  description: "蓝衫ChatGPT进阶版是OpenAI基于GPT-4架构的升级型语言生成模型。它继承了GPT-3的强大生成能力，并通过更大的模型规模和优化的算法，实现了更高质量的文本生成、更准确的信息获取和更自然的对话交互。蓝衫ChatGPT进阶版能更好地理解和生成复杂、多样化的文本，为用户提供更加丰富和个性化的体验。",
+  title: "ChatGPT 进阶版",
+  description: "ChatGPT进阶版是OpenAI基于GPT-4架构的升级型语言生成模型。它继承了GPT-3的强大生成能力，并通过更大的模型规模和优化的算法，实现了更高质量的文本生成、更准确的信息获取和更自然的对话交互。蓝衫ChatGPT进阶版能更好地理解和生成复杂、多样化的文本，为用户提供更加丰富和个性化的体验。",
   viewport: {
     width: "device-width",
     initialScale: 1,
